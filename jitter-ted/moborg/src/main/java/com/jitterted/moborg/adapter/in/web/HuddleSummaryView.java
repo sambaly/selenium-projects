bc3c16b3715f09c82cf4b57c331d1bce646c1e80
sambaly <kote.sambaly@gmail.com>
2023-10-02 14:23:08 +0000

    Created Huddle and HuddleService in the dashboard.

--- jitter-ted/moborg/src/main/java/com/jitterted/moborg/adapter/in/web/HuddleSummaryView.java
+++ jitter-ted/moborg/src/main/java/com/jitterted/moborg/adapter/in/web/HuddleSummaryView.java
@@ -1,6 +1,22 @@
 package com.jitterted.moborg.adapter.in.web;
 
+import com.jitterted.moborg.domain.Huddle;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
 public record HuddleSummaryView(String name,
                                 String dateTime,
                                 int numberRegistered) {
+    public static List<HuddleSummaryView> from(List<Huddle> huddles) {
+        return huddles.stream()
+                .map(HuddleSummaryView::toView)
+                .toList();
+    }
+
+    private static HuddleSummaryView toView(Huddle huddle) {
+        return new HuddleSummaryView(huddle.name(), huddle.startDateTime().toString(), huddle.numberRegistered());
+    }
+
+
 }
