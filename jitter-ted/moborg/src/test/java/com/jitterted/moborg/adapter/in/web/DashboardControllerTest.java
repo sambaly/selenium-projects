49479c803aa247418eb1ceba0158bb86c6a931d4
sambaly <kote.sambaly@gmail.com>
2023-10-09 11:59:45 +0000

    Return 404 from huddle detail view when the huddle ID is not found.

--- jitter-ted/moborg/src/test/java/com/jitterted/moborg/adapter/in/web/DashboardControllerTest.java
+++ jitter-ted/moborg/src/test/java/com/jitterted/moborg/adapter/in/web/DashboardControllerTest.java
@@ -5,12 +5,13 @@ import com.jitterted.moborg.domain.HuddleRepository;
 import com.jitterted.moborg.domain.HuddleService;
 import com.jitterted.moborg.domain.InMemoryHuddleRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.ConcurrentModel;
 import org.springframework.ui.Model;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.ZonedDateTime;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -75,8 +76,9 @@ class DashboardControllerTest {
 
         assertThatThrownBy(() -> {
             dashboardController.huddleDetailView(model, 0L);
-        }).isInstanceOf(NoSuchElementException.class);
-
+        }).isInstanceOf(ResponseStatusException.class)
+                .extracting("status")
+                .isEqualTo(HttpStatus.NOT_FOUND);
 
     }
 
