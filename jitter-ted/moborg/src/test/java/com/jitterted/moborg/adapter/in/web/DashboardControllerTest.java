8b30459a80f7b217cc779c86b441795d028f1ed6
sambaly <kote.sambaly@gmail.com>
2023-10-09 11:39:54 +0000

    Added huddle ID as path variable to huddle detail view.

--- jitter-ted/moborg/src/test/java/com/jitterted/moborg/adapter/in/web/DashboardControllerTest.java
+++ jitter-ted/moborg/src/test/java/com/jitterted/moborg/adapter/in/web/DashboardControllerTest.java
@@ -1,6 +1,7 @@
 package com.jitterted.moborg.adapter.in.web;
 
 import com.jitterted.moborg.domain.Huddle;
+import com.jitterted.moborg.domain.HuddleRepository;
 import com.jitterted.moborg.domain.HuddleService;
 import com.jitterted.moborg.domain.InMemoryHuddleRepository;
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,10 @@ import org.springframework.ui.Model;
 
 import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DashboardControllerTest {
 
@@ -43,6 +46,38 @@ class DashboardControllerTest {
                 .isEqualTo("redirect:/dashboard");
         assertThat(huddleRepository.findAll())
                 .hasSize(1);
+    }
+
+    @Test
+    public void detailViewOfExistingHuddleByItsIdIsReturned() throws Exception {
+        HuddleRepository huddleRepository = new InMemoryHuddleRepository();
+        Huddle savedHuddle = huddleRepository.save(new Huddle("Huddle #1", ZonedDateTime.now()));
+        HuddleService huddleService = new HuddleService(huddleRepository);
+        DashboardController dashboardController = new DashboardController(huddleService);
+
+        Model model = new ConcurrentModel();
+        dashboardController.huddleDetailView(model, 0L);
+
+        HuddleDetailView huddle = (HuddleDetailView) model.getAttribute("huddle");
+
+        assertThat(huddle.name())
+                .isEqualTo(savedHuddle.name());
+    }
+
+    @Test
+    public void detailViewOfNonExistentHuddleReturns404NotFound() throws Exception {
+        HuddleRepository huddleRepository = new InMemoryHuddleRepository();
+        // Huddle savedHuddle = huddleRepository.save(new Huddle("Huddle #1", ZonedDateTime.now()));
+        HuddleService huddleService = new HuddleService(huddleRepository);
+        DashboardController dashboardController = new DashboardController(huddleService);
+
+        Model model = new ConcurrentModel();
+
+        assertThatThrownBy(() -> {
+            dashboardController.huddleDetailView(model, 0L);
+        }).isInstanceOf(NoSuchElementException.class);
+
+
     }
 
 }
